f994674a3def7900f47c65bf2ded096ada4ffb9b
Simon Shields <simon@lineageos.org>
2017-09-04 21:11:16 +1000

    build: soong: put lineage variables in the "Evervolv" struct
    Change-Id: I9ed55cea0c55f0078f8f97ad4037b16ed11c0156

--- soong/soong_config.mk
+++ soong/soong_config.mk
@@ -1,9 +1,12 @@
+# Insert new variables inside the Evervolv structure
 evervolv_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '"Evervolv": {'; \
 	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
 	echo '    "Has_legacy_mmap": $(if $(filter true,$(BOARD_USES_LEGACY_MMAP)),true,false),'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
