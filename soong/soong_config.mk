3a98c3eac3c48ca84422210a452770e965280425
Stricted <info@stricted.net>
2018-02-17 17:28:52 +0100

    lineage: soong: replace space with colon on TARGET_LD_SHIM_LIBS
    that way we can split the shim list logically into multiple lines like
    
    TARGET_LD_SHIM_LIBS := /system/bin/gpsd|/vendor/lib/libshim_gpsd.so \
        /system/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so \
        /system/lib/libstagefright.so|/system/lib/libstagefright_shim.so
    
    to not have one single huge line
    
    [lineage-17.0]
    this got lost in:
        soong: rework soong_config for Pie
        Change-Id: I3c5001f02975e5487ba66a02217f1d24d2021272
    
    Change-Id: Ice1411ee22b78da1dbf22eac412b9e4978acebdf

--- soong/soong_config.mk
+++ soong/soong_config.mk
@@ -16,7 +16,7 @@ $(call add_json_bool, Needs_non_pie_support, $(filter true,$(TARGET_NEEDS_NON_PI
 $(call add_json_bool, Needs_prelink_support, $(filter true,$(TARGET_NEEDS_PRELINK_SUPPORT)))
 $(call add_json_str_omitempty, Target_init_vendor_lib, $(TARGET_INIT_VENDOR_LIB))
 $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
-$(call add_json_str_omitempty, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
+$(call add_json_str_omitempty, Target_shim_libs, $(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
 $(call add_json_bool, Uses_media_extensions, $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)))
 $(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
 $(call add_json_bool, Uses_qti_camera_device, $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
