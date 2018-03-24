93bf4f2d88500bb09f02e236cbec424073bdba85
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-24 11:17:49 -0700

    soong: Add TARGET_USES_QTI_CAMERA_DEVICE
    Change-Id: I925b137edeed9fa11bbe566002147b444328c661

--- soong/android/variable.go
+++ soong/android/variable.go
@@ -57,6 +57,11 @@ type Product_variables struct {
 	Uses_qcom_bsp_legacy struct {
 		Cppflags []string
 	}
+
+	Uses_qti_camera_device struct {
+		Cppflags []string
+		Shared_libs []string
+	}
 }
 
 type ProductVariables struct {
@@ -72,4 +77,5 @@ type ProductVariables struct {
 	Target_shim_libs  *string `json:",omitempty"`
 	Uses_media_extensions   *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
+	Uses_qti_camera_device  *bool `json:",omitempty"`
 }
