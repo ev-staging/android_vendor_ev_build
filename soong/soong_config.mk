5144a41c2720e806dfce20fa7f1691f712c15c5d
Bruno Martins <bgcngm@gmail.com>
2018-08-27 10:08:54 +0100

    soong_config: Remove extra spacing
     * Let's not rewrite the whole list while adding new vars
    
    Change-Id: Icab6116839bbb74a75d207310562faf9d780828b

--- soong/soong_config.mk
+++ soong/soong_config.mk
@@ -4,15 +4,15 @@ _contents := $(_contents)    "Evervolv":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
-$(call add_json_bool, Has_legacy_camera_hal1,                $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
-$(call add_json_bool, Has_legacy_mmap,                       $(filter true,$(BOARD_USES_LEGACY_MMAP)))
-$(call add_json_bool, Needs_egl_khr_workaround,              $(filter true,$(BOARD_EGL_WORKAROUND_BUG_10194508)))
-$(call add_json_bool, Needs_non_pie_support,                 $(filter true,$(TARGET_NEEDS_NON_PIE_SUPPORT)))
-$(call add_json_bool, Needs_prelink_support,                 $(filter true,$(TARGET_NEEDS_PRELINK_SUPPORT)))
+$(call add_json_bool, Has_legacy_camera_hal1, $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
+$(call add_json_bool, Has_legacy_mmap, $(filter true,$(BOARD_USES_LEGACY_MMAP)))
+$(call add_json_bool, Needs_egl_khr_workaround, $(filter true,$(BOARD_EGL_WORKAROUND_BUG_10194508)))
+$(call add_json_bool, Needs_non_pie_support, $(filter true,$(TARGET_NEEDS_NON_PIE_SUPPORT)))
+$(call add_json_bool, Needs_prelink_support, $(filter true,$(TARGET_NEEDS_PRELINK_SUPPORT)))
 $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
-$(call add_json_str,  Target_shim_libs,                      $(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
-$(call add_json_bool, Uses_media_extensions,                 $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)))
-$(call add_json_bool, Uses_qcom_bsp_legacy,                  $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
+$(call add_json_str,  Target_shim_libs, $(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
+$(call add_json_bool, Uses_media_extensions, $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)))
+$(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END
