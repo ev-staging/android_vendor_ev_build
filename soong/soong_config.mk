87a3e791927c2991b547c12a0fca884363a05483
Paul Keith <javelinanddart@gmail.com>
2018-09-21 00:52:58 +0200

    ev: Use add_json_str_omitempty directive for TARGET_LD_SHIM_LIBS
    * The code is ifdef'ed out, so let's match that expecation here
    
    Change-Id: I59c8e276c3dd28c4f46222f66d626e48b2aa6551

--- soong/soong_config.mk
+++ soong/soong_config.mk
@@ -10,7 +10,7 @@ $(call add_json_bool, Needs_egl_khr_workaround, $(filter true,$(BOARD_EGL_WORKAR
 $(call add_json_bool, Needs_non_pie_support, $(filter true,$(TARGET_NEEDS_NON_PIE_SUPPORT)))
 $(call add_json_bool, Needs_prelink_support, $(filter true,$(TARGET_NEEDS_PRELINK_SUPPORT)))
 $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
-$(call add_json_str,  Target_shim_libs, $(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
+$(call add_json_str_omitempty, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
 $(call add_json_bool, Uses_media_extensions, $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)))
 $(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
 
