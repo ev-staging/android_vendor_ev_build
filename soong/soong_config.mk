8bc224209d37d904200bd604b2bc4f1d7d0e6712
Nick Reuter <nreuter85@gmail.com>
2019-09-04 17:41:56 -0500

    soong: update for use of json map
    Change-Id: I11b9623a77868ce5016fe0b5dc6748ba1632af82

--- soong/soong_config.mk
+++ soong/soong_config.mk
@@ -1,6 +1,6 @@
 add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 
-_contents := $(_contents)    "Evervolv":{$(newline)
+$(call add_json_map, Evervolv)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
@@ -21,7 +21,4 @@ $(call add_json_bool, Uses_media_extensions, $(filter true,$(TARGET_USES_MEDIA_E
 $(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
 $(call add_json_bool, Uses_qti_camera_device, $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
 
-# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
-_contents := $(_contents)__SV_END
-
-_contents := $(_contents)    },$(newline)
+$(call end_json_map)
