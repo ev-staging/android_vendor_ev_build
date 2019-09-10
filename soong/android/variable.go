8347f7ded6c40eeabb3cb9c262dc8ac76922ac79
Han Wang <416810799@qq.com>
2019-09-10 13:08:04 +0200

    soong_config: Add new flag for vendor_init
    Change-Id: I383f1de230ecf7b79d78fe701b140bd1bc6795f2

--- soong/android/variable.go
+++ soong/android/variable.go
@@ -46,6 +46,10 @@ type Product_variables struct {
 		Cppflags []string
 	}
 
+	Target_init_vendor_lib struct {
+		Whole_static_libs []string
+	}
+
 	Target_process_sdk_version_override struct {
 		Cppflags []string
 	}
@@ -78,6 +82,7 @@ type ProductVariables struct {
 	Needs_egl_khr_workaround  *bool `json:",omitempty"`
 	Needs_non_pie_support  *bool `json:",omitempty"`
 	Needs_prelink_support  *bool `json:",omitempty"`
+	Target_init_vendor_lib  *string `json:",omitempty"`
 	Target_process_sdk_version_override *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 	Uses_media_extensions   *bool `json:",omitempty"`
